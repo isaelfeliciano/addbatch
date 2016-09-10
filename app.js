332ca12800eeef312a58eb10454df04d4e074769
Isael Feliciano <rogelioisael@hotmail.com>
2016-09-10 01:54:13 -0400

    Add modal
    Add modal for at first time of use users enter their name

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 $('input[name="number-input"]').number(true, 3);
 $('input[name="quantity"]').number(true, 3);
+
+$('#btn-create-page').on('click', function(e) {
+    window.location = "#create-page";
+});
