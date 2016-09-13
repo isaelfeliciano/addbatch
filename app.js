6220994ec9d7f7125d6f95f6398f345983037280
Isael Feliciano <rogelioisael@hotmail.com>
2016-09-12 22:28:34 -0400

    Fix issue with modal
    I was binding to events to the same element

--- app.js
+++ app.js
@@ -2,36 +2,57 @@ const lS = localStorage;
 let ticketsValues = [];
 
 function updateNumberFormat() {
-	$('input[name="number-input"]').number(true);
+	$('input[name="number-input"]').number(true, 3);
 	$('input[name="quantity"]').number(true, 3);
 };
 updateNumberFormat();
 
 $('#btn-create-page').on('click', (e) => {
+	e.preventDefault();
     window.location = "#create-page";
     $('.modal__box i').attr('class', 'fa fa-5x fa-book').text('');
     $('#jsModalInput').attr('name', 'batchNumber');
     $('#jsModalInput').attr('placeholder', 'Enter batch number');
-    $('.modal').toggleClass('no-display');
+    $('.modal').removeClass('no-display');
     $('#jsModalInput').focus();
     $('.jsModal-save').on('click', (e) => {
-    	let batchNumber = $('#jsModalInput').val();
-    	$('#jsBatchNumber').text(batchNumber);
-    	$('.modal').toggleClass('no-display');
-	    $('input[name="number-input"]').focus();
+    	e.preventDefault();
+    	btnModalSaveNewBatch();
     });
 });
 
+$('#jsModalInput').on('keydown', (e) => {
+	if (e.which == 13 || e.keyCode == 13) {
+		if ($('.modal__box i').hasClass('fa-book')) {
+			btnModalSaveNewBatch();
+		} else {
+			btnModalSaveNewUser();
+		}
+	}
+});
+
+function btnModalSaveNewBatch() {
+	let batchNumber = $('#jsModalInput').val();
+	$('#jsBatchNumber').text(batchNumber);
+	$('.modal').addClass('no-display');
+  	$('input[name="number-input"]').focus();
+}
+
+function btnModalSaveNewUser() {
+	let userName = $('input[name="user"]').val();
+	lS.setItem('firstTime', `{userName: ${userName}}`);
+	$('.modal').addClass('no-display');
+	$('input[name="user"]').val('');
+	$('input[name="search"]').focus();
+}
+
 // lS.removeItem('firstTime');
 if (!lS.getItem('firstTime')) {
-	$('.modal').toggleClass('no-display');
+	$('.modal').removeClass('no-display');
 	$('input[name="user"]').focus();
-	$('.jsModal-save').on('click', function() {
-		let userName = $('input[name="user"]').val();
-		lS.setItem('firstTime', `{userName: ${userName}}`);
-		$('.modal').toggleClass('no-display');
-		$('input[name="user"]').val('');
-		$('input[name="search"]').focus();
+	$('.jsModal-save').on('click', function(e) {
+		e.preventDefault();
+		btnModalSaveNewUser();
 	});
 }
 
@@ -57,16 +78,7 @@ $('input[name="number-input"]').on('keydown', (e) => {
 
 		value = parseFloat(value);
 		ticketsValues.push(value);
-		console.log(value);
 		$('#jsTotal').text(_.sum(ticketsValues));
 		$('#jsTotal').number(true, 3);
-		console.log(ticketsValues);
-	}
-});
-
-$('#jsModalInput').on('keydown', (e) => {
-	if (e.which == 13 || e.keyCode == 13) {
-		e.preventDefault();
-		$('.jsModal-save').trigger('click');
 	}
 });
